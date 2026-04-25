3b94421f4336503e41b138e0fd7c98b0af1d508f
babu-lang <danlindo2007@gmail.com>
2026-04-25 12:29:13 -0300

    telasDanilo

---
+++ src/main/java/org/example/partidas/partidas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.example.partidas;
+
+/**
+ *
+ * @author babu
+ */
+public class partidas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
